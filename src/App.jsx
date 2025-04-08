69527ee10ff85c5b69d99cddde05db664abe9e21
radavilae <raul.davila.esp@gmail.com>
2025-04-08 15:18:04 +0200

    done

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import SimpleList from "./Components/SimpleList";
+import StudentList from "./Components/StudentList";
+import MovieList from "./Components/MovieList";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <List /> */}
+      {/* <StudentList /> */}
+      {/* <ProjectList /> */}
+
+      <MovieList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/SimpleList.jsx
@@ -0,0 +1,13 @@
+const numbersList = [<li>1</li>, <li>2</li>, <li>3</li>, <li>4</li>];
+
+function SimpleList() {
+  return (
+    <div className="list">
+      <h2>List</h2>
+
+      {numbersList}
+    </div>
+  );
+}
+
+export default SimpleList;
